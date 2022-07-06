3712e4ddd85e85c801702539cc2ee689b190dbdb
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-06 09:53:49 +0200

    add page transitions to mobile view

--- lib/src/shared/providers/theme.dart
+++ lib/src/shared/providers/theme.dart
@@ -149,7 +149,7 @@ class ThemeProvider extends InheritedWidget {
   ThemeData light([Color? targetColor]) {
     final colorScheme = colors(Brightness.light, targetColor);
     return ThemeData.light().copyWith(
-      // Add page transitions
+      pageTransitionsTheme: pageTransitionsTheme,
       colorScheme: colorScheme,
       appBarTheme: appBarTheme(colorScheme),
       cardTheme: cardTheme(),
@@ -167,7 +167,7 @@ class ThemeProvider extends InheritedWidget {
   ThemeData dark([Color? targetColor]) {
     final colorScheme = colors(Brightness.dark, targetColor);
     return ThemeData.dark().copyWith(
-      // Add page transitions
+      pageTransitionsTheme: pageTransitionsTheme,
       colorScheme: colorScheme,
       appBarTheme: appBarTheme(colorScheme),
       cardTheme: cardTheme(),
